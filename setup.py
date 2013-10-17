6ae874c9b78422ba57215fe8718bc5b7e4c5366f
Dan F-M <danfm@nyu.edu>
2013-10-17 01:41:51 +0000

    version and more cleanup

--- setup.py
+++ setup.py
@@ -5,6 +5,10 @@ from setuptools import setup
 
 setup(
     name="osrc",
+    version="2.0.0",
+    author="Daniel Foreman-Mackey",
+    author_email="danfm@nyu.edu",
+    url="http://osrc.dfm.io",
     packages=["osrc"],
     package_data={"osrc": ["*.json", "templates/*", "static/*"]},
     include_package_data=True,
