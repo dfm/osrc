1ffb44237633e917db945252771fe49fef8fde95
Dan F-M <danfm@nyu.edu>
2015-07-29 11:34:15 -0700

    making scripts

--- create_app.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-from osrc import create_app
-dirname = os.path.dirname(os.path.abspath(__file__))
-app = create_app(os.path.join(dirname, "local.py"))
